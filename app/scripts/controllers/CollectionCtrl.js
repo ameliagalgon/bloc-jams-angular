85193ce5b18c4761c57352aa4969c6576da393e8
ameliagalgon <amg617@lehigh.edu>
2017-08-09 11:07:12 -0400

    Assignment 5 Controllers: complete

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,7 @@
 (function(){
     function CollectionCtrl(){
-        console.log("Collection controller: ");
         this.albums = [];
         for(var i = 0; i < 12; i++){
-            console.log("pushing album into array");
             this.albums.push(angular.copy(albumPicasso));
         }
     }
